3622ed64f47f04462e485241399a1919aac0202a
tcely <tcely@users.noreply.github.com>
2025-07-05 19:04:40 -0400

    fixup: consistent capitalization

--- huey_django_orm/apps.py
+++ huey_django_orm/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoOrmHueyConfig(AppConfig):
+class DjangoORMHueyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_huey_orm"
