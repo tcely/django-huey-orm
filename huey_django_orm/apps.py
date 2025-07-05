fdde85fe290fa80ae3bdd3f4b0df980821c49ba7
tcely <tcely@users.noreply.github.com>
2025-07-05 19:02:22 -0400

    Update apps.py

--- huey_django_orm/apps.py
+++ huey_django_orm/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HueyDjangoOrmConfig(AppConfig):
+class DjangoOrmHueyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "huey_django_orm"
+    name = "django_huey_orm"
