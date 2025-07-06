e9ae3af16d4a4675fd5ecf3f286a86b4d1d055b2
tcely <tcely@users.noreply.github.com>
2025-07-06 00:30:39 -0400

    Update __init__.py

--- django_huey_orm/__init__.py
+++ django_huey_orm/__init__.py
@@ -1,9 +1,18 @@
 import logging
 
 from .huey import DjangoORMHuey
-from .vars import NAME as name # noqa: F401
-from .version import VERSION as __version__ # noqa: F401
+from .storage import DjangoORMStorage
+from .vars import NAME as name
+from .version import VERSION as __version__
 
 logger = logging.getLogger(__name__)
+
+__all__ = [
+  'name',
+  '__version__',
+  'logger',
+  'DjangoORMHuey',
+  'DjangoORMStorage',
+]
 
 
