0c1c4d5dae8c7e4fe63a5a5303682010addd4e1e
tcely <tcely@users.noreply.github.com>
2025-07-05 20:15:08 -0400

    Update __init__.py

--- huey_django_orm/__init__.py
+++ huey_django_orm/__init__.py
@@ -7,6 +7,8 @@ logger = logging.getLogger(__name__)
 name = "django-huey-orm"
 __version__ = "0.0.1"
 
+prefix = "huey_django_orm"
+
 
 class DjangoORMHuey(Huey):
     storage_class = DjangoORMStorage
